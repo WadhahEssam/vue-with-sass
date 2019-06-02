e2573407edd42c6eecea9942f2f5e237cedc82fa
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-06-03 01:59:11 +0300

    Initial commit

---
+++ src/components/New.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="not-main">
+    this is the new component
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        color: 'red'
+      }
+    }
+    
+  }
+</script>
+
+<style lang="sass" scoped>
+  @import '../Sass/app'
+  .not-main
+    color: $black
+    font-family: monospace
+</style>
